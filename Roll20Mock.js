702c8090cf0551c71caf4838ef09ec23a7d632a6
davout1805 <jeir9BLcUDsb>
2013-10-30 00:37:35 -0400

    ConditionsTest.js Roll20Mock.js

---
+++ Roll20Mock.js
@@ -0,0 +1,23 @@
+/*
+ * These are needed for mocking framework.
+ */
+
+var state = {};
+
+function on(eventCondition, func) {
+}
+
+function log(str) {
+}
+
+function sendChat(speakingAs, input) {
+}
+
+function findObjs(criteriaObj) {
+}
+
+function randomInteger(max){
+}
+
+function getObj(type, id) {
+}
